32025d62b399540c3ef400fc0fbd0ce66b5f4c15
Thibaud Chominot <thibaud.chominot@epita.fr>
2019-03-31 22:31:38 +0200

    utils: add singleton templates

---
+++ include/utils/singleton.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+template <class T>
+class SingletonBase {
+protected:
+  struct SingletonExclusiveConstructor {};
+
+  SingletonBase(SingletonExclusiveConstructor);
+  SingletonBase(const SingletonBase&) = delete;
+  SingletonBase(SingletonBase&&) = delete;
+  SingletonBase& operator=(const SingletonBase&) = delete;
+  SingletonBase& operator=(SingletonBase&&) = delete;
+};
+
+template <class T>
+class Singleton : SingletonBase<T> {
+public:
+  static const T& instance() {
+    static T static_instance(SingletonExclusiveConstructor{});
+    return static_instance;
+  };
+};
+
+template <class T>
+class MutableSingleton : SingletonBase<T> {
+public:
+  static T& instance() {
+    static T static_instance(SingletonExclusiveConstructor{});
+    return static_instance;
+  };
+};
